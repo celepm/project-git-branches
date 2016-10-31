476072d32841ba1bd60b5fdf3d5569281446516d
Mustafa Celep <mcelep@gmail.com>
2016-10-31 14:48:39 +0100

    I just added a println on the main method.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This line I added from dev branch");
+		System.out.println("this line was added from master branch");
 	}
 
 }
