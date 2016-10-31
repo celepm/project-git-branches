ef0a94cb559e40e1a8787aadbc904dfb7a035b67
Mustafa Celep <mcelep@gmail.com>
2016-10-31 14:59:25 +0100

    commit from dev branch. I just added a line in the code in order to merge.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This line I added from dev branch");
 		System.out.println("this line was added from master branch");
+		System.out.println("This line I added from dev branch to perform a merge.");
 	}
 
 }
